129d1714975c5cf9bdc3fec994099e1a5d58b4a5
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-12 09:57:16 +0700

    Created Model

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# Task Model
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    status = models.BooleanField(default=False) # ให้ค่าเริ่มต้นเป็น fault
