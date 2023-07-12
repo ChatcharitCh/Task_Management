9febe6db017aa42404f8a5d2a1f191ff5d68c6e8
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-12 10:07:35 +0700

    Created Admin

--- taskapp/admin.py
+++ taskapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from taskapp.models import Task
 
+# Customized Admin
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "status"]
+    
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
+
