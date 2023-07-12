cb82110bf47970ea1e07f4894ff1a06d33d87dd9
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-12 12:01:10 +0700

    Created Function Status Task

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -3,4 +3,6 @@
 from django.urls import path
 from taskapp import views
 
-urlpatterns = [path('',views.index)]
+urlpatterns = [path('',views.index),
+               path('completed/<task_id>', views.complete_task, name = "completed"), # ตั้งชื่อ path = completed
+               path('pending/<task_id>', views.pending_task, name = "pending")] 
--- taskapp/views.py
+++ taskapp/views.py
@@ -22,3 +22,17 @@ def index(request):
     else :
         all_task = Task.objects.all()  # ดึงข้อมูลทั้งหมดมา
         return render(request, "index.html", {"all_task": all_task}) 
+
+def complete_task(request, task_id):
+    task = Task.objects.get(pk = task_id)
+    task.status = True # เปลี่ยนสถานะของงาน
+    task.save()
+    # messages.success(request, "Saved Successfully")
+    return redirect("/")
+
+def pending_task(request, task_id):
+    task = Task.objects.get(pk = task_id)
+    task.status = False # เปลี่ยนสถานะของงาน
+    task.save()
+    # messages.success(request, "Saved Successfully")
+    return redirect("/")
