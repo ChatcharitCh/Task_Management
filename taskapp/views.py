57b4d7c0472ec79b0fb1eace600d4edac40f931a
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-12 10:59:11 +0700

    Show Data

--- taskapp/views.py
+++ taskapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from taskapp.models import Task
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    all_task = Task.objects.all()  # ดึงข้อมูลทั้งหมดมา
+    return render(request, "index.html", {"all_task": all_task}) 
