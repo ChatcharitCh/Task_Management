866e831e2eeb3133f93eca1e1eaac125b896e943
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-20 22:06:08 +0700

    User Access Control

--- taskapp/views.py
+++ taskapp/views.py
@@ -18,12 +18,13 @@ def index(request):
             return redirect("/")
         # Save
         else:
-            task = Task.objects.create(name = name) # เอามาแต่ชื่อ
+            task = Task.objects.create(name = name, manager=request.user) 
             task.save()
             messages.success(request, "Saved Successfully")
             return redirect("/")
     else :
-        all_task = Task.objects.all()  # ดึงข้อมูลทั้งหมดมา
+        # all_task = Task.objects.all()  # ดึงข้อมูลทั้งหมดมา
+        all_task = Task.objects.filter(manager=request.user) # ดึงข้อมูลของแต่ละ manager
         page = request.GET.get("page")
         paginator = Paginator(all_task, 5) # กำหนดรายการที่แสดงต่อ 1 หน้า
         all_task = paginator.get_page(page)
@@ -32,15 +33,24 @@ def index(request):
 @login_required(login_url="/login")
 def complete_task(request, task_id):
     task = Task.objects.get(pk = task_id)
-    task.status = True # เปลี่ยนสถานะของงาน
-    task.save()
-    # messages.success(request, "Saved Successfully")
-    return redirect("/")
+    # User access control
+    if task.manager == request.user:
+        task.status = True # เปลี่ยนสถานะของงาน
+        task.save()
+        # messages.success(request, "Saved Successfully")
+        return redirect("/")
+    else:
+        messages.warning(request, "You dont have permission")
+        return redirect("/")
 
 @login_required(login_url="/login")
 def pending_task(request, task_id):
     task = Task.objects.get(pk = task_id)
-    task.status = False # เปลี่ยนสถานะของงาน
-    task.save()
-    # messages.success(request, "Saved Successfully")
-    return redirect("/")
+    if task.manager == request.user:
+        task.status = False # เปลี่ยนสถานะของงาน
+        task.save()
+        # messages.success(request, "Saved Successfully")
+        return redirect("/")
+    else:
+        messages.warning(request, "You dont have permission")
+        return redirect("/")
